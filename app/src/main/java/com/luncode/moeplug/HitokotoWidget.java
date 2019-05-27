d1367b86f5cea75a33b7342a6e73d825aa5e0f09
Luncode <1370655052@qq.com>
2019-05-27 18:36:37 +0800

    暂时完善小插件一言显示功能

--- app/src/main/java/com/luncode/moeplug/HitokotoWidget.java
+++ app/src/main/java/com/luncode/moeplug/HitokotoWidget.java
@@ -3,23 +3,45 @@ package com.luncode.moeplug;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+
+import android.os.Handler;
+import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.luncode.moeplug.Util.HttpUtil;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Response;
+
 
 /**
  * Implementation of App Widget functionality.
  */
 public class HitokotoWidget extends AppWidgetProvider {
+    private static String Url="https://v1.hitokoto.cn/?c=b";
+    static  RemoteViews views = new RemoteViews("com.luncode.moeplug", R.layout.hitokoto_widget);
+    static void updateAppWidget(Context context, final AppWidgetManager appWidgetManager, final int appWidgetId) {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        //CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.hitokoto_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        //views.setTextViewText(R.id.hitokoto_id,"00001");
+        Hitokoto();
+        appWidgetManager.updateAppWidget(appWidgetId,views);
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Hitokoto();
+                appWidgetManager.updateAppWidget(appWidgetId,views);
+                handler.postDelayed(this,30*1000);
+            }
+        };
+        runnable.run();
     }
 
     @Override
@@ -38,6 +60,34 @@ public class HitokotoWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+    private static void parseJSONWithGSON(String jsonData){
+        Gson gson = new Gson();
+        HitokotoModel model = gson.fromJson(jsonData,HitokotoModel.class);
+        Log.d("jsonhandler","ID:"+model.getId()
+                +"\tTEXT:"
+                +model.getHitokoto()
+                +"\tto:"+model.getFrom()
+                +"\tCreator:"+model.getCreator());
+        setHikoto(model.getId(),model.getHitokoto(),model.getFrom(),model.getCreator());
+    }
+    private static void Hitokoto(){
+        HttpUtil.sendrequestHttp(Url,new okhttp3.Callback(){
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseData=response.body().string();
+                parseJSONWithGSON(responseData);
+            }
+        });
+    }
+    private static void setHikoto(String id,String text,String form,String Creator){
+        views.setTextViewText(R.id.hitokoto_id,"ID:"+id);
+        views.setTextViewText(R.id.hitokoto_text,text);
+        views.setTextViewText(R.id.hitokoto_from,"TO:"+form);
+        views.setTextViewText(R.id.hitokoto_Creator,"Creator:"+Creator);
     }
 }
 
--- app/src/main/java/com/luncode/moeplug/MainActivity.java
+++ app/src/main/java/com/luncode/moeplug/MainActivity.java
@@ -1,8 +1,12 @@
 package com.luncode.moeplug;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -20,26 +24,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        HttpUtil.sendrequestHttp("https://v1.hitokoto.cn/?c=b",new okhttp3.Callback(){
-
-            @Override
-            public void onFailure(Call call, IOException e) {
-            }
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                    String responseData=response.body().string();
-                    parseJSONWithGSON(responseData);
-                    //Log.d("json",responseData);
-            }
-        });
-    }
-    private void parseJSONWithGSON(String jsonData){
-        Gson gson = new Gson();
-        HitokotoModel model = gson.fromJson(jsonData,HitokotoModel.class);
-        Log.d("jsonex","ID:"+model.getId()
-                +"\tTEXT:"
-                +model.getHitokoto()
-                +"\tto:"+model.getFrom()
-                +"\t"+model.getCreator());
     }
 }
